d2ecb6dc97e4b6d10f79caf18c2c9e7369a4c563
Jing Zhou <zhoujing2227@gmail.com>
2019-08-20 14:37:16 -0700

    fix addLast() if emptyList

---
+++ 2_lists/SLList2.java
@@ -0,0 +1,63 @@
+public class SLList2 {
+    public class IntNode {
+        public int item;
+        public IntNode next;
+        public IntNode(int i, IntNode n) {
+            item = i;
+            next = n;
+        }
+    }
+
+    private IntNode first; 
+    private int size;
+
+    public SLList2() {
+        first = null;
+        size = 0;
+    }
+
+    public SLList2(int x) {
+        first = new IntNode(x, null);
+        size = 1;
+    }
+
+    /** Adds an item to the front of the list. */
+    public void addFirst(int x) {
+        first = new IntNode(x, first);
+        size += 1;
+    }    
+
+    /** Retrieves the front item from the list. */
+    public int getFirst() {
+        return first.item;
+    }
+
+    /** Returns the number of items in the list. */
+    public int size() {
+        return size;
+    }
+
+    /** Adds an item to the end of the list. */
+    public void addLast(int x) {
+        size += 1;
+        IntNode p = first;
+
+        if (p == null) {
+            first = new IntNode(x, null);
+            return;
+        }
+
+        /* Advance p to the end of the list. */
+        while (p.next != null) {
+            p = p.next;
+        }
+        p.next = new IntNode(x, null);
+    }
+
+    /** Crashes when you call addLast on an empty SLList. Fix it. */
+    public static void main(String[] args) {
+        SLList2 x = new SLList2();
+        x.addLast(5);
+        System.out.println(x.size());
+    }
+}
